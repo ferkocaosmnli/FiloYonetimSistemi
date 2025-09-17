1d4db99ff0dc690a875aea57abc2ba058377b9e4
Ferhat Kocaosmanli <ferkocaosmnli@gmail.com>
2025-09-17 16:36:17 +0300

    Update DBConnection.java

--- src/filotakip_app/DBConnection.java
+++ src/filotakip_app/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/filo_takip";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Tyle1903.";
+    private static final String USER = "...";
+    private static final String PASSWORD = "...";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
